1828e0f01f56a001125134c98521b139931aa59c
SMHRD <SMHRD@DESKTOP-E3M0DBA>
2020-06-22 11:36:21 +0900

    로그인

---
+++ Team/src/view/MainGUI.java
@@ -0,0 +1,23 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class MainGUI {
+
+	private JFrame frame;
+
+	public MainGUI() {
+		initialize();
+		frame.setVisible(true);
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
