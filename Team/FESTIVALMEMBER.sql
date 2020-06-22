d61c93e1bcb447edb4c56bbeff42c54e0e70f596
SMHRD <SMHRD@DESKTOP-E3M0DBA>
2020-06-22 10:36:18 +0900

    회원 정보 테이블

---
+++ Team/FESTIVALMEMBER.sql
@@ -0,0 +1,14 @@
+CREATE TABLE FESTIVALMEMBER (
+	id varchar2(100),
+	pw varchar2(100),
+	name varchar2(100),
+	phone varchar2(100),
+	location varchar2(100),
+	sex varchar2(100),
+	age varchar2(100)
+)
+SELECT * FROM FESTIVALMEMBER;
+
+DELETE FROM FESTIVALMEMBER;
+
+DROP TABLE FESTIVALMEMBER;
