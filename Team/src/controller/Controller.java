bf190244d786a9b4f95f40142b15c77b3fe05105
SMHRD <SMHRD@DESKTOP-BF6G508>
2020-06-23 14:41:47 +0900

    정보 수정 기능 추가

--- Team/src/controller/Controller.java
+++ Team/src/controller/Controller.java
@@ -6,10 +6,31 @@ import model.MemberVO;
 public class Controller {
 	private MemberDAO dao = new MemberDAO();
 	private MemberVO loginUser;
+	private MemberVO joinUser;
+	private MemberVO joinMember;
+	
+	public MemberVO getJoinMember() {
+		return joinMember;
+	}
+	
+	
+	public MemberVO getJoinUser() {
+		return joinUser;
+	}
 
 	public MemberVO getLoginUser() {
 		return loginUser;
 	}
+	
+	public boolean member(MemberVO member) {
+		joinMember = dao.select(member);
+		if (joinMember != null) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
 
 	public boolean login(MemberVO user) {
 		loginUser = dao.selectOne(user);
@@ -19,11 +40,22 @@ public class Controller {
 			return false;
 		}
 	}
+	
+	public MemberVO select(MemberVO member) {
+		joinMember = dao.select(member);
+		if (joinMember != null) {
+			return joinMember;
+		} else {
+			return null;
+		}
+	}
 
 	public int join(MemberVO joinUser) {
 		int cnt = dao.insert(joinUser);
 		return cnt;
 	}
+	
+
 
 	public boolean check(String id) {
 		String check_ID;
@@ -34,4 +66,5 @@ public class Controller {
 			return false;
 		}
 	}
+
 }
