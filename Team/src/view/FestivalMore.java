c56f506636fd940bb9a1f351e7de2d0807380c0e
SMHRD <SMHRD@DESKTOP-ACA9HD2>
2020-06-25 18:31:29 +0900

    뒤로가기창 활성화

--- Team/src/view/FestivalMore.java
+++ Team/src/view/FestivalMore.java
@@ -1,11 +1,18 @@
 package view;
 
+import java.awt.Button;
+import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.SpringLayout;
 
 import org.jfree.chart.ChartFactory;
@@ -17,19 +24,22 @@ import org.jfree.data.category.DefaultCategoryDataset;
 
 import model.ChartReadCSV;
 import model.ChartVO;
-
-import java.awt.Color;
-import javax.swing.JLabel;
-import java.util.ArrayList;
-import javax.swing.SwingConstants;
-import java.awt.Button;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import model.FestivalInformationDAO;
+import model.listVO;
+import view.FestivalInformation;
 
 public class FestivalMore {
 
 	private JFrame frame;
-
+	int age10 = 0;
+	int age20 = 0;
+	int age30 = 0;
+	int age40 = 0;
+	int cnt10 = 0;
+	int cnt20 = 0;
+	int cnt30 = 0;
+	int cnt40 = 0;
+	String name = null;
 	/**
 	 * Launch the application.
 	 */
@@ -61,66 +71,128 @@ public class FestivalMore {
 		frame.setResizable(false);
 		frame.setBounds(100, 100, 450, 700);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new GridLayout(0,1,0,0));
-		
+		frame.getContentPane().setLayout(new GridLayout(0, 1, 0, 0));
+
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.WHITE);
 		frame.getContentPane().add(panel);
-		SpringLayout sl_panel = new SpringLayout();
-		panel.setLayout(sl_panel);
+		
+		name = "센텀맥주축제";
 		
 		ChartReadCSV readCSV = new ChartReadCSV();
-		ArrayList<ChartVO> list = readCSV.getList();
+		ArrayList<ChartVO> list = readCSV.getList(name);
+		
+		double score10 = 0;
+		double score20 = 0;
+		double score30 = 0;
+		double score40 = 0;
+		int cnt10 = 0;
+		int cnt20 = 0;
+		int cnt30 = 0;
+		int cnt40 = 0;
+	
+	
+		for (int i = 0; i < list.size(); i++) {
+			if(list.get(i).getAge() == 10) {
+				score10 += list.get(i).getRating();
+				cnt10++;
+			}else if(list.get(i).getAge() == 20) {
+				score20 += list.get(i).getRating();
+				cnt20++;
+			}else if(list.get(i).getAge() == 30) {
+				score30 += list.get(i).getRating();
+				cnt30++;
+			}else {
+				score40 += list.get(i).getRating();
+				cnt40++;
+			}
+		}
+		
+	
+		listVO voo10 = new listVO(score10/cnt10,"10대");
+		listVO voo20 = new listVO(score20/cnt20,"20대");
+		listVO voo30 = new listVO(score30/cnt30,"30대");
+		listVO voo40 = new listVO(score40/cnt40,"40대");
+		
+	
+		ArrayList<listVO> list2 = new ArrayList<listVO>();
+		list2.add(voo10);
+		list2.add(voo20);
+		list2.add(voo30);
+		list2.add(voo40);
+	
+	
 		
 		DefaultCategoryDataset dataset1 = new DefaultCategoryDataset();
 		
-		for(int i = 0; i < list.size(); i++) {
-			dataset1.addValue(list.get(i).getUsage(),list.get(i).getMonth(),list.get(i).getDivision());		
+		
+		
+		for (int i = 0; i < list2.size(); i++) {
+			dataset1.addValue(list2.get(i).getScore(), "역대 축제평점", list2.get(i).getAge()+"");
 		}
 		
-		JFreeChart barChart = ChartFactory.createBarChart("연령대 평점","연령대","평점",dataset1, PlotOrientation.VERTICAL,true,true,true);
-		//한글을 적용하기 위한 Font 객체 생성
-		// 1.폰트종류 2.폰트타입 3.크기
-		Font f = new Font("Gulim",Font.BOLD, 10);
-		barChart.getTitle().setFont(f);
+		
+		
 		
 	
+		
+		
+		// 그래프를 그려주는 객체 dataset을 넣어준다
+		// 1.제목 2.x축 제목 3.y축제목 4.그래프 데이터
+		// 5. 그래프 가로로 할건지 세로로 할건지
+		// 6,7,8 그래프의 기능 넣을것인지
+		JFreeChart barChart = ChartFactory.createBarChart(name,"연령대","평점",dataset1, PlotOrientation.VERTICAL,false,true,true);
+		
+		//한글을 적용하기 위한 Font 객체 생성
+		// 1.폰트종류 2.폰트타입 3.크기
+		Font f = new Font("Gulim",Font.BOLD, 14);
+		barChart.getTitle().setFont(f);
+		
+		
 		CategoryPlot plot1 = barChart.getCategoryPlot();
 		plot1.getDomainAxis().setLabelFont(f);
 		plot1.getDomainAxis().setTickLabelFont(f);
 		plot1.getRangeAxis().setLabelFont(f);
 		plot1.getRangeAxis().setTickLabelFont(f);
+		panel.setLayout(null);
+		//파이차트
+		
+		
 		
 		
 		ChartPanel chartPanel = new ChartPanel(barChart);
-		
-		sl_panel.putConstraint(SpringLayout.NORTH, chartPanel, 64, SpringLayout.NORTH, panel);
+		chartPanel.setBounds(10,64,422,300);
 		chartPanel.setPreferredSize(new java.awt.Dimension(300,300));
-		sl_panel.putConstraint(SpringLayout.WEST, chartPanel, 41, SpringLayout.WEST, panel);
-		sl_panel.putConstraint(SpringLayout.EAST, chartPanel, -57, SpringLayout.EAST, panel);
 		panel.add(chartPanel);
-		chartPanel.setLayout(new SpringLayout());
+		chartPanel.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("\uD6C4\uAE30");
-		sl_panel.putConstraint(SpringLayout.SOUTH, chartPanel, -6, SpringLayout.NORTH, lblNewLabel);
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		sl_panel.putConstraint(SpringLayout.NORTH, lblNewLabel, -425, SpringLayout.SOUTH, panel);
-		sl_panel.putConstraint(SpringLayout.WEST, lblNewLabel, 41, SpringLayout.WEST, panel);
-		sl_panel.putConstraint(SpringLayout.SOUTH, lblNewLabel, -10, SpringLayout.SOUTH, panel);
-		sl_panel.putConstraint(SpringLayout.EAST, lblNewLabel, 377, SpringLayout.WEST, panel);
-		panel.add(lblNewLabel);
+		
+		FestivalInformationDAO dao = new FestivalInformationDAO();
+		
+		ArrayList<String> review = dao.getReview(name);
+		
+		String[] header = {"후기"};
+		String[][] data = new String[review.size()][1];
+		for (int i = 0; i < data.length; i++) {
+			data[i][0] = review.get(i);
+		}
+		frame.setVisible(true);
 		
 		Button button_back = new Button("\u25C0");
+		button_back.setBounds(10, 10, 60, 48);
 		button_back.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				frame.setVisible(false);
-//				FestivalInformation.main(null);
+				frame.dispose();
+				FestivalInformation info = new FestivalInformation();
 			}
 		});
-		sl_panel.putConstraint(SpringLayout.NORTH, button_back, 10, SpringLayout.NORTH, panel);
-		sl_panel.putConstraint(SpringLayout.WEST, button_back, 41, SpringLayout.WEST, panel);
-		sl_panel.putConstraint(SpringLayout.SOUTH, button_back, 43, SpringLayout.NORTH, panel);
-		sl_panel.putConstraint(SpringLayout.EAST, button_back, 102, SpringLayout.WEST, panel);
 		panel.add(button_back);
+		JTable table = new JTable(data, header);
+		table.setFillsViewportHeight(true);
+		table.setSurrendersFocusOnKeystroke(true);
+		table.setFont(new Font("굴림", Font.PLAIN, 20));
+		JScrollPane scrollPane = new JScrollPane(table);
+		scrollPane.setBounds(10, 374, 422, 234);
+		panel.add(scrollPane);
 	}	
 }
