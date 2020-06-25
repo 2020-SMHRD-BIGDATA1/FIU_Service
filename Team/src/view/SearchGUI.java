c2d670f1dc5e2f60a0c06643c6e4d84f56e086a6
SMHRD <SMHRD@DESKTOP-NBLK11D>
2020-06-25 16:45:56 +0900

    검색창 삭제 재수정

--- Team/src/view/SearchGUI.java
+++ Team/src/view/SearchGUI.java
@@ -4,7 +4,6 @@ import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -101,7 +100,7 @@ public class SearchGUI {
 		// 검색버튼 끝
 
 		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(12, 95, 410, 45);
+		panel_1.setBounds(12, 108, 410, 45);
 		panel.add(panel_1);
 		panel_1.setLayout(new GridLayout(0, 3, 0, 0));
 
@@ -115,7 +114,7 @@ public class SearchGUI {
 				age.setVisible(false);
 			}
 		});
-
+ 
 		panel_1.add(btn_area);
 
 		JButton btn_date = new JButton("\uB0A0\uC9DC");
@@ -146,9 +145,20 @@ public class SearchGUI {
 		panel_1.add(btn_age);
 
 		result_panel = new JPanel();
-		result_panel.setBounds(12, 174, 410, 452);
+		result_panel.setBounds(12, 199, 410, 452);
 		panel.add(result_panel);
 		result_panel.setLayout(cl_result_panel);
+
+	
+		search = new JPanel();
+		result_panel.add(search, "name_65526670868800");
+		search.setLayout(new CardLayout(0, 0));
+
+		JScrollPane scrollPane_1 = new JScrollPane();
+		search.add(scrollPane_1, "name_71518854043800");
+
+		table_2 = new JTable();
+		scrollPane_1.setViewportView(table_2);
 
 		// 지역별 검색결과
 		area = new JPanel();
@@ -160,103 +170,60 @@ public class SearchGUI {
 		area.add(areaList);
 		areaList.setLayout(new GridLayout(17, 1, 0, 0));
 		
-//		MapPanel mapPanel = new MapPanel();
-//		mapPanel.setPath("C:\\Users\\SMHRD\\git\\repository\\Team\\img\\전국.PNG");
-		 
 		JButton btn_1 = new JButton("\uC11C\uC6B8");
-//		btn_1.addMouseListener(new MouseAdapter() {
-//			@Override
-//			public void mouseEntered(MouseEvent e) {
-//				mapPanel.setVisible(false);
-//				mapPanel.setPath("C:\\Users\\SMHRD\\git\\repository\\Team\\img\\서울.PNG");
-//				mapPanel.repaint();
-//				mapPanel.setVisible(true);
-//			}
-//
-//			@Override
-//			public void mouseExited(MouseEvent e) {
-//				mapPanel.setVisible(false);
-//				mapPanel.setPath("C:\\Users\\SMHRD\\git\\repository\\Team\\img\\전국.PNG");
-//				mapPanel.repaint();
-//				mapPanel.setVisible(true);
-//			}
-//		});
-		btn_1.setBackground(Color.WHITE);
 		areaList.add(btn_1);
 		
 		JButton btn_2 = new JButton("\uACBD\uAE30");
-		btn_2.setBackground(Color.WHITE);
 		areaList.add(btn_2);
 		
 		JButton btn_3 = new JButton("\uC778\uCC9C");
-		btn_3.setBackground(Color.WHITE);
 		areaList.add(btn_3);
 		
 		JButton btn_4 = new JButton("\uC138\uC885");
-		btn_4.setBackground(Color.WHITE);
 		areaList.add(btn_4);
 		
 		JButton btn_5 = new JButton("\uB300\uC804");
-		btn_5.setBackground(Color.WHITE);
 		areaList.add(btn_5);
 		
 		JButton btn_6 = new JButton("\uB300\uAD6C");
-		btn_6.setBackground(Color.WHITE);
 		areaList.add(btn_6);
 		
 		JButton btn_7 = new JButton("\uAD11\uC8FC");
-		btn_7.setBackground(Color.WHITE);
 		areaList.add(btn_7);
 		
 		JButton btn_8 = new JButton("\uC6B8\uC0B0");
-		btn_8.setBackground(Color.WHITE);
 		areaList.add(btn_8);
 		
 		JButton btn_9 = new JButton("\uBD80\uC0B0");
-		btn_9.setBackground(Color.WHITE);
 		areaList.add(btn_9);
 		
 		JButton btn_10 = new JButton("\uAC15\uC6D0");
-		btn_10.setBackground(Color.WHITE);
 		areaList.add(btn_10);
 		
 		JButton btn_11 = new JButton("\uCDA9\uBD81");
-		btn_11.setBackground(Color.WHITE);
 		areaList.add(btn_11);
 		
 		JButton btn_12 = new JButton("\uCDA9\uB0A8");
-		btn_12.setBackground(Color.WHITE);
 		areaList.add(btn_12);
 		
 		JButton btn_13 = new JButton("\uACBD\uBD81");
-		btn_13.setBackground(Color.WHITE);
 		areaList.add(btn_13);
 		
 		JButton btn_14 = new JButton("\uACBD\uB0A8");
-		btn_14.setBackground(Color.WHITE);
 		areaList.add(btn_14);
 		
 		JButton btn_15 = new JButton("\uC804\uBD81");
-		btn_15.setBackground(Color.WHITE);
 		areaList.add(btn_15);
 		
 		JButton btn_16 = new JButton("\uC804\uB0A8");
-		btn_16.setBackground(Color.WHITE);
 		areaList.add(btn_16);
 		
 		JButton btn_17 = new JButton("\uC81C\uC8FC");
-		btn_17.setBackground(Color.WHITE);
 		areaList.add(btn_17);
 		
 		JPanel mapPanel = new JPanel();
-		
 		mapPanel.setBounds(98, 0, 312, 452);
 		area.add(mapPanel);
-		
-//		MapPanel map_panel = new MapPanel();
-		
-//		mapPanel.setPath("C:\\Users\\SMHRD\\git\\repository\\Team\\img\\전국.PNG");
-		
 
 		// 지역별 출력문
 //		DefaultTableModel model = new DefaultTableModel(header, 0);
