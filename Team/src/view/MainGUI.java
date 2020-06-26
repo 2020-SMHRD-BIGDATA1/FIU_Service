4075296cb5972e445db5bf0a29ff9151cf5eb4ca
SMHRD <SMHRD@DESKTOP-NBLK11D>
2020-06-26 09:46:17 +0900

    메인 이미지 update

--- Team/src/view/MainGUI.java
+++ Team/src/view/MainGUI.java
@@ -3,6 +3,7 @@ package view;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.GraphicsEnvironment;
 
 import javax.swing.ImageIcon;
@@ -15,11 +16,13 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.JTextArea;
 import javax.swing.JSeparator;
+import javax.imageio.ImageIO;
 import javax.swing.DropMode;
 //import com.jgoodies.forms.factories.DefaultComponentFactory;
 
 import java.awt.CardLayout;
 import javax.swing.border.BevelBorder;
+import javax.swing.border.LineBorder;
 import javax.swing.JEditorPane;
 import javax.swing.JList;
 import javax.swing.JSpinner;
@@ -30,6 +33,11 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.MatteBorder;
+import java.awt.SystemColor;
 
 public class MainGUI {
 
@@ -80,29 +88,60 @@ public class MainGUI {
 		panel.add(lblNewLabel);
 
 		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(0, 181, 434, 105);
+		panel_1.setBackground(Color.WHITE);
+		panel_1.setBounds(0, 181, 434, 100);
 		panel.add(panel_1);
 		panel_1.setLayout(null);
 
-		JPanel panel_2 = new JPanel();
-		panel_2.setBounds(0, 0, 100, 105);
-		panel_1.add(panel_2);
+		JPanel event1 = new JPanel() {
+			public void paintComponent(Graphics g){
+				  g.fillRect(0, 0, 60, 100);
+				  Image image = new ImageIcon("C:\\Users\\SMHRD\\git\\repository\\Team\\img\\event1.PNG").getImage();
+				  g.drawImage(image,0,0,this);
+				 }
+				};
+		event1.setBounds(0, 0, 100, 100);
+		panel_1.add(event1);
 
-		JPanel panel_3 = new JPanel();
-		panel_3.setBounds(113, 0, 100, 105);
-		panel_1.add(panel_3);
+		JPanel event2 = new JPanel(){
+			public void paintComponent(Graphics g){
+			  g.fillRect(0, 0, 60, 100);
+			  Image image = new ImageIcon("C:\\Users\\SMHRD\\git\\repository\\Team\\img\\event2.PNG").getImage();
+			  g.drawImage(image,0,0,this);
+			}
+		};
+		event2.setBounds(113, 0, 100, 100);
+		panel_1.add(event2);
 
-		JPanel panel_4 = new JPanel();
-		panel_4.setBounds(225, 0, 100, 105);
-		panel_1.add(panel_4);
+		JPanel event3 = new JPanel(){
+			public void paintComponent(Graphics g){
+				  g.fillRect(0, 0, 60, 100);
+				  Image image = new ImageIcon("C:\\Users\\SMHRD\\git\\repository\\Team\\img\\event3.PNG").getImage();
+				  g.drawImage(image,0,0,this);
+				}
+			};
+		event3.setBounds(225, 0, 100, 100);
+		panel_1.add(event3);
 
-		JPanel panel_4_1 = new JPanel();
-		panel_4_1.setBounds(334, 0, 100, 105);
-		panel_1.add(panel_4_1);
+		JPanel event4 = new JPanel(){
+			public void paintComponent(Graphics g){
+				  g.fillRect(0, 0, 60, 100);
+				  Image image = new ImageIcon("C:\\Users\\SMHRD\\git\\repository\\Team\\img\\event4.PNG").getImage();
+				  g.drawImage(image,0,0,this);
+				}
+			};
+		event4.setBounds(334, 0, 100, 100);
+		panel_1.add(event4);
 
-		JPanel panel_5 = new JPanel();
-		panel_5.setBounds(0, 334, 434, 258);
-		panel.add(panel_5);
+		JPanel main1 = new JPanel(){
+			public void paintComponent(Graphics g){
+				  Image image = new ImageIcon("C:\\Users\\SMHRD\\git\\repository\\Team\\img\\gofest.PNG").getImage();
+				  g.drawImage(image,0,0,this);
+				}
+			};
+		main1.setBounds(0, 334, 434, 258);
+		panel.add(main1);
+		main1.setBorder(new MatteBorder(0, 0, 1, 0, (Color) new Color(0, 191, 255)));
 
 		JLabel lblNewLabel_1 = new JLabel("  \uCD95\uC81C NOW");
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.LEFT);
@@ -120,12 +159,23 @@ public class MainGUI {
 		lbl_logo.setBounds(0, 0, 75, 75);
 		panel.add(lbl_logo);
 
-		JPanel panel_6 = new JPanel();
-		panel_6.setBounds(0, 83, 434, 33);
-		panel.add(panel_6);
-		panel_6.setLayout(new GridLayout(0, 3, 0, 0));
+		JPanel menu_bar = new JPanel();
+		menu_bar.setBounds(0, 83, 434, 33);
+		panel.add(menu_bar);
+		menu_bar.setLayout(new GridLayout(0, 3, 0, 0));
 
-		JButton btn_review = new JButton("\uD6C4\uAE30");
+		JButton btn_review = new JButton("");
+		btn_review.setIcon(new ImageIcon("C:\\Users\\SMHRD\\git\\repository\\Team\\img\\\uD6C4\uAE30.png"));
+		btn_review.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				btn_review.setIcon(new ImageIcon("C:\\Users\\SMHRD\\git\\repository\\Team\\img\\\uD6C4\uAE302.png"));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				btn_review.setIcon(new ImageIcon("C:\\Users\\SMHRD\\git\\repository\\Team\\img\\\uD6C4\uAE30.png"));
+			}
+		});
 		btn_review.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				ReviewGUI reviewGUI = new ReviewGUI();
@@ -133,9 +183,20 @@ public class MainGUI {
 			}
 		});
 		
-				JButton btn_search = new JButton("\uAC80\uC0C9");
+				JButton btn_search = new JButton("");
+				btn_search.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseEntered(MouseEvent e) {
+						btn_search.setIcon(new ImageIcon("C:\\Users\\SMHRD\\git\\repository\\Team\\img\\\uAC80\uC0C92.png"));
+					}
+					@Override
+					public void mouseExited(MouseEvent e) {
+						btn_search.setIcon(new ImageIcon("C:\\Users\\SMHRD\\git\\repository\\Team\\img\\\uAC80\uC0C9.png"));
+					}
+				});
+				btn_search.setIcon(new ImageIcon("C:\\Users\\SMHRD\\git\\repository\\Team\\img\\\uAC80\uC0C9.png"));
 				btn_search.setFont(new Font("»ﬁ∏’∆Ì¡ˆ√º", Font.PLAIN, 15));
-				panel_6.add(btn_search);
+				menu_bar.add(btn_search);
 				btn_search.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						SearchGUI searchGUI = new SearchGUI();
@@ -148,9 +209,20 @@ public class MainGUI {
 		btn_review.setFont(new Font("»ﬁ∏’∆Ì¡ˆ√º", Font.PLAIN, 15));
 		btn_review.setBorderPainted(false);
 		btn_review.setBackground(Color.WHITE);
-		panel_6.add(btn_review);
+		menu_bar.add(btn_review);
 		
-				JButton btn_my = new JButton("My FEST");
+				JButton btn_my = new JButton("");
+				btn_my.setIcon(new ImageIcon("C:\\Users\\SMHRD\\git\\repository\\Team\\img\\myfest.png"));
+				btn_my.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseEntered(MouseEvent e) {
+						btn_my.setIcon(new ImageIcon("C:\\Users\\SMHRD\\git\\repository\\Team\\img\\myfest2.png"));
+					}
+					@Override
+					public void mouseExited(MouseEvent e) {
+						btn_my.setIcon(new ImageIcon("C:\\Users\\SMHRD\\git\\repository\\Team\\img\\myfest.png"));
+					}
+				});
 				btn_my.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent arg0) {
 					MyPage mypage = new MyPage();
@@ -160,7 +232,7 @@ public class MainGUI {
 				btn_my.setFont(new Font("»ﬁ∏’∆Ì¡ˆ√º", Font.BOLD, 15));
 				btn_my.setBorderPainted(false);
 				btn_my.setBackground(Color.WHITE);
-				panel_6.add(btn_my);
+				menu_bar.add(btn_my);
 				
 				JLabel lblNewLabel_2 = new JLabel("\uC9C4\uD589\uC911\uC778 \uC774\uBCA4\uD2B8");
 				lblNewLabel_2.setFont(new Font("»ﬁ∏’∆Ì¡ˆ√º", Font.PLAIN, 12));
